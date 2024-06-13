338b24fbee1762bd6e8ea135c8f87f140278d1a4
rayhaanxo <147975786+rayhaanxo@users.noreply.github.com>
2024-06-13 14:58:59 +0000

    learnt about data types

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,20 @@
+//alert(3+3) //we are using nodejs not browser
+console.log(3+3)
+//readability should be high 
+console.log("rayhaanxo");
+
+let name="rayhaanxo" //string
+let age=19//number
+let isloggedin=false //boolean 
+
+//number datatype range  2 to the power 53
+//bigint for stocks and trading hft
+//string 
+//boolean true and false
+//null is a standalone value / is a representation of empty
+//undefined when the value is not defined /when the value is not assigned
+//symbol => unique
+
+
+//object 
+
---
+++ 01_basics/variables.js
@@ -0,0 +1,9 @@
+const accountId = 14455.  //const keyword changes is not allowed
+let accountEmail = "rayhaankhan616@gmail.com"
+var accountPassword = "12345"
+accountcity = "mumbai"
+ console.log(accountId);
+ console.table([accountEmail,accountPassword,accountcity]);
+ /*
+ prefer not to use var because of issue in block scope and functional scope 
+ */
