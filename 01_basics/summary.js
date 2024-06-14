fb15320d0b166d82383ec321126a790a68cb4d45
rayhaanxo <147975786+rayhaanxo@users.noreply.github.com>
2024-06-14 15:19:12 +0000

    datatypes summary

---
+++ 01_basics/summary.js
@@ -0,0 +1,33 @@
+//primitive datatypes
+// 7 types: 
+//String =>call by value 
+// number 
+//boolean
+//null =>empty
+// undefined 
+//symbol
+//BigInt
+ 
+
+// Reference type (non primitive datatype)
+//arrays
+//object
+//functions
+
+
+
+//arrays
+
+const heroes =["captain america", "thor","stark","snape"]
+
+//obj
+let myObj={
+    age: 18,
+    name:"rayhaanxo"
+}
+
+//functions 
+let  myfunctions= function(){
+    console.log("rayhaan");
+
+ }
