0755cb5de990186bfa490fa128b92925cef01353
rayhaanxo <147975786+rayhaanxo@users.noreply.github.com>
2024-06-19 22:32:29 +0000

    strings

---
+++ 01_basics/strings.js
@@ -0,0 +1,19 @@
+// const name = "rayhaan"
+// const count = 3
+// console.log(`my name is ${name} and count is ${count}`);
+//accessing keys of the strings 
+
+
+const game = new String("rayhaan-xo")
+console.log(game[0]);
+
+// console.log(game.length);
+// console.log(game.toUpperCase())
+// console.log(game.charAt(2));
+// console.log(game.indexOf("x"));
+
+const newstrings = game.substring(0,7)
+// console.log(newstrings);
+
+const anotherstring = game.slice(3,6)
+console.log(anotherstring);
